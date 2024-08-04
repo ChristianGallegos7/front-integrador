409903058b389a1435c3ce4837e012d8e98bd427
Pxlarized <christian@moon.ec>
2024-08-04 18:20:33 -0500

    YA SE REGISTRA EL USUARIO DESDE EL FRONT

--- src/app/auth/user/register-page/register-page.component.ts
+++ src/app/auth/user/register-page/register-page.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '@services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
+
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe(
+        response => {
+          console.log('Registro exitoso', response);
+        },
+        error => {
+          console.error('Error en el registro', error);
+        }
+      );
+    }
+  }
 
 }
